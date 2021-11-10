9ada651a06390acc0b44059d24078ead407d31ce
Namiki-M <m.namiki.carecon@gmail.com>
2021-11-10 19:35:33 +0900

    属性で設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
+
+    <x-tests.card title="タイトル" content="コンテンツ"></x-tests.card>
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー２</x-slot>
 コンポーネントテスト2
+
+<x-tests.card title="aiueko" content="atatata"></x-tests.card>
 </x-tests.app>
