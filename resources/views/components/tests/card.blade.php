50ccc080d5b00dc20434721a5032b877e9d29174
Namiki-M <m.namiki.carecon@gmail.com>
2021-11-10 20:08:03 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,7 +1,14 @@
+@props([
+    'title',
+    'message' => 'メッセージ初期値です',
+    'message1' => 'メーっせーじ',
+    'content' => '本文の初期値です'
+    ])
+
 <div class="border-2 shadow w-1/2 p-2">
  <div>{{ $title }}</div>
  <div>画像</div>
  <div> {{ $content }} </div>
  <div> {{ $message }} </div>
-  <div> {{ $message1 }} </div>
+ <div> {{ $message1 }} </div>
 </div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
 コンポーネントテスト１
 
     <x-tests.card title="タイトル" content="コンテンツ" :message="$message" :message1="$message1"></x-tests.card>
+    <x-tests.card title="タイトル２"></x-tests.card>
 </x-tests.app>
