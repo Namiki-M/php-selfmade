e74502bc1eff26be37e3fa6b2133eb6091dd02b5
Namiki-M <m.namiki.carecon@gmail.com>
2021-11-29 00:37:18 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,9 +4,35 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            
+            $id = $request->route()->parameter('product'); 
+            if(!is_null($id)){ // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id; //productモデルはownerと繋がっていないため、shopモデルを挟む
+                $productId = (int)$productsOwnerId; //$productsOwnerIdはstring型なので、(int)キャストで数字に変える。
+                if($productId !== Auth::id()){ 
+                    abort(404); // 404画面表示
+                }
+            }
+         
+
+            return $next($request);
+        });
+
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +40,22 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        //ログインしているオーナーが作ったproductを取得
+        // $products = Owner::findOrFail(Auth::id())
+        // ->shop->product;
+        // 「N + 1」問題が発生しているため、修正↓
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id', Auth::id())->get();
+
+        // dd($ownerInfo);
+        // foreach($ownerInfo as $owner){
+        //     // dd($owner->shop->product);
+        //     foreach($owner->shop->product as $product){
+        //         dd($product->imageFirst->filename);
+        //     }
+        // }
+        return view('owner.products.index', 
+        compact('ownerInfo'));
     }
 
     /**
