cb834dad46cae423b6fd80199e3a298313541a36
Namiki-M <m.namiki.carecon@gmail.com>
2021-11-29 01:04:50 +0900

    stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_stocks'; //テーブル名を変える時はこの記述が必須
+}
