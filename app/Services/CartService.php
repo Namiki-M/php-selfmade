4f75805d1b555ac75fd7e31b8908c532f343c579
Namiki-M <m.namiki.carecon@gmail.com>
2021-12-19 21:37:26 +0900

    CartService作成

---
+++ app/Services/CartService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\Models\Cart;
+
+
+class CartService{
+
+    public static function getItemsInCart($items){
+        $products = [];
+
+        
+        foreach($items as $item){
+            $p = Product::findOrFail($item->product_id);
+            $owner = $p->shop->owner->select('name', 'email')->first()->toArray(); //toArray配列に変換するメソッド
+            $values = array_values($owner);
+            $keys = ['ownerName','email'];
+            $ownerInfo = array_combine($keys, $values);//オーナー情報のキー変更
+            
+            $product = Product::where('id', $item->product_id) //商品情報の配列
+            ->select('id','name','price')->get()->toArray();
+
+            $quantity = Cart::where('product_id', $item->product_id) //在庫数の配列
+            ->select('quantity')->get()->toArray();
+
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]);
+
+            array_push($products, $result);
+        }
+        dd($products);
+        return $products;
+    }
+}
