cbe99035cb40efc52c4772fc4f77a33ff7da8530
Namiki-M <m.namiki.carecon@gmail.com>
2021-12-23 18:37:22 +0900

    その他設定

--- resources/views/user/auth/reset-password.blade.php
+++ resources/views/user/auth/reset-password.blade.php
@@ -1,10 +1,13 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <div class="w-28">
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
         </x-slot>
+        
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
